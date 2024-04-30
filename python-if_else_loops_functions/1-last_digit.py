9440b54b8f951bd261e8b4ce94332c46a930459a
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-30 19:27:34 +0000

    updated if loop with two conditions, added strings 4 and 5

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,13 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 str1 = "Last digit of"
 str2 = "is"
-neg = "-"
+str3 = "and is greater than 5"
+str4 = "and is less than 6 and not 0"
+str5 = "and is 0"
 if number < 0:
-    print (str1, number, str2, neg, last_digit, sep=' ')
+    if last_digit == 0:
+        print (str1, number, str2, (last_digit * -1), str5, sep=' ')
+    else:
+        print (str1, number, str2, (last_digit * -1) ,str4, sep=' ')
 else:
     print (str1, number, str2, last_digit, sep=' ')
