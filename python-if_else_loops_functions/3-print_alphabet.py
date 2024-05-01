03cef7969244554a8f38931942bc9f5afe5dd973
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-01 16:46:08 +0000

    created 3-print_alphabet.py clone of 2-print_alphabet.py adds if condition for 101 and 113 and passes

---
+++ python-if_else_loops_functions/3-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+ascii_value = 96
+while ascii_value < 123:
+    ascii_value += 1
+    if (ascii_value == 101 or ascii_value == 113):
+        pass
+    else:
+        print("{}".format(chr(ascii_value)), end='')
