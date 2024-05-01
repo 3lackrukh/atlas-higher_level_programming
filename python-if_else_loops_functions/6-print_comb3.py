d6eb900290e51f59eacc139b8772785fcf9b1de7
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-01 19:09:59 +0000

    created 6-print_comb3.py specify range, if 10's is greater than 1's place, skip, print otherwise, 89prints without a comma.

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(1,90):
+    if (i / 10 % 10 > i % 10):
+        pass
+    elif (i == 90 - 1):
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}".format(i), end=', ')
