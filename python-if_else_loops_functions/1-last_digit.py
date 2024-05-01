02e438db140b59bf203f150662862acf7b6e04cf
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-01 13:57:59 +0000

    removed whitespace on newline 22, before ( lines 12, 14, 17, 19, 21

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,14 +9,13 @@ str4 = "and is less than 6 and not 0"
 str5 = "and is 0"
 if number < 0:
     if last_digit == 0:
-        print (str1, number, str2, (last_digit * -1), str5, sep=' ')
+        print(str1, number, str2, (last_digit * -1), str5, sep=' ')
     else:
-        print (str1, number, str2, (last_digit * -1) ,str4, sep=' ')
+        print(str1, number, str2, (last_digit * -1), str4, sep=' ')
 else:
     if last_digit > 5:
-        print (str1, number, str2, last_digit, str3, sep=' ')
+        print(str1, number, str2, last_digit, str3, sep=' ')
     elif last_digit == 0:
-        print (str1, number, str2, last_digit, str5, sep=' ')
+        print(str1, number, str2, last_digit, str5, sep=' ')
     else:
-        print (str1, number, str2, last_digit, str4, sep=' ')
-        
+        print(str1, number, str2, last_digit, str4, sep=' ')
