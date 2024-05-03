2654da652d8465c2bcad8aaef1f2dff601836949
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-03 01:54:42 +0000

    changed logic in else condition to arg_ct - 1 line 11

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print(f"1: {sys.argv[1]}")
     else:
-        print(f"{arg_ct} arguments:")
+        print(f"{arg_ct - 1} arguments:")
         for i, arg in enumerate(sys.argv[1:], start=1):
             print(f"{i}: {arg}")
