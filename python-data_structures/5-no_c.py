c7dfb7fff4d34226cb26096785e019b14b44ad3e
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-04 02:00:49 +0000

    shortened names to decrease line length line 3

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''.join([char for char in my_string if char != 'c' and char != 'C'])
-    return new_string
+    new_str = ''.join([c for c in my_string if c != 'c' and c != 'C'])
+    return new_str
