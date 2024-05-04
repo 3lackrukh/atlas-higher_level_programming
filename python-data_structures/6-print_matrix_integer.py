64ac8b6665a915924c79838ada6ce1c3cbc8069b
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-04 02:36:45 +0000

    corrected ambiguous variable naming line 5, added missing whitespace lines 8:39, 10:39

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         n = 1
-        l = len(row)
+        ln = len(row)
         for i in row:
-            if n == l:
-                print("{:d}".format(i),end='')
+            if n == ln:
+                print("{:d}".format(i), end='')
             else:
-                 print("{:d}".format(i),end=' ')
+                print("{:d}".format(i), end=' ')
             n += 1
         print()
