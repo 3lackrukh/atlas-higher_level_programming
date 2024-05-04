5cec514107eaa5885f3db4a5f0075957d5f3d3e6
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-04 04:16:41 +0000

    changed max_length to 2 suiting the parameters of the problem

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    max_len = max(len(tuple_a), len(tuple_b))
+    max_len = 2
 
     tuple_a = tuple_a + (0,) * (max_len - len(tuple_a))
     tuple_b = tuple_b + (0,) * (max_len - len(tuple_b))
