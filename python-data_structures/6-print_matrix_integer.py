8bb667a7e6010d6969515f1236e1ddb4d5ca77c8
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-04 02:34:00 +0000

    added variables for length and a condition lines 7-10 to leave off whitespace on last int in row.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for item in row:
-            print("{:d}".format(item),end=' ')
+        n = 1
+        l = len(row)
+        for i in row:
+            if n == l:
+                print("{:d}".format(i),end='')
+            else:
+                 print("{:d}".format(i),end=' ')
+            n += 1
         print()
