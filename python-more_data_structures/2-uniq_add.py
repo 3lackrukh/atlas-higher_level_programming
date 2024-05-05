299f12caf5e7709ceedf09c4451eb912a092f31d
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-05 17:45:42 +0000

    created 2-main.py and 2-uniq_add.py initializes int result and set seen. for loop checks if int at index is in set seen. If not, adds to result.

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    seen = set()
+    for i in my_list:
+        if i not in seen:
+            result += i
+            seen.add(i)
+    return result
+  
