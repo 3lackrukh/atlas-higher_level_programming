b2ff4c7294e59ff031fc144bd400130b36f7635c
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-06 13:46:37 +0000

    created 10-main.py and 10-best_score.py checks for edge case: empty dictionary and dictionary with no stored values. initializes high_score to first non-empty value, compares against all other values, updating high_score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary or not any(a_dictionary.values()):
+        return None
+    high_score = next(value for value in a_dictionary.values() if value is not None)
+    for value in a_dictionary.values():
+        if value is None:
+            continue
+        if value > high_score:
+            high_score = value
+    return high_score
