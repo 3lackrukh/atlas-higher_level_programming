9e404c16eb9a1394782d2bcd768bb95008ccc32d
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-06 13:08:11 +0000

    defined new dictionary, a copy of original. executed multiplication on copied dictionary

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] *= 2
-    return a_dictionary
+    b_dict = a_dictionary.copy()
+    for key in b_dict:
+        b_dict[key] *= 2
+    return b_dict
