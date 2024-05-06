b80cfb7a4e97d48784daeaf59e23fa4d90aecb5e
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-06 14:05:38 +0000

    modified for loop line 6 yielding key, value pairs, new variable champ = corresponding key to high_score.

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,9 +3,10 @@ def best_score(a_dictionary):
     if not a_dictionary or not any(a_dictionary.values()):
         return None
     high_score = next(value for value in a_dictionary.values() if value is not None)
-    for value in a_dictionary.values():
+    for key, value in a_dictionary.items():
         if value is None:
             continue
-        if value > high_score:
+        if value >= high_score:
             high_score = value
-    return high_score
+            champ = key
+    return champ
