2e4c19bec0946bd98649c391cbaa3c61c9490d14
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-06 19:39:05 +0000

    added edge case: no string lines 5,6

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_num = 0
+    if not roman_string:
+        return None 
     prev = rom_num[roman_string[0]]
     for i in roman_string:
         int_num += rom_num[i]
