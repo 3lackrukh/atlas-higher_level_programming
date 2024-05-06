15e8ad88de8dda49e219a521d71d1fa590816f15
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-06 19:56:57 +0000

    adjusted variable name lines 4,9,11,15

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_num = 0
+    total = 0
     if not roman_string or not isinstance(roman_string, str):
         return 0
     prev = rom_num[roman_string[0]]
     for i in roman_string:
-        int_num += rom_num[i]
+        total += rom_num[i]
         if prev < rom_num[i]:
-            int_num -= prev * 2
+            total -= prev * 2
         prev = rom_num[i]
-    return int_num
+    return total
