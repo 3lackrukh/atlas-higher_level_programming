80ea46d837206b20e89f34f32eb3449c644e3885
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 17:45:54 +0000

    created 2-square.py and test file. makes size optional, raises type and value errors for not an int and size > 0

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -13,8 +13,13 @@ class Square:
         size (int): length of the sides.
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Initialize new Square instance.
         """
-        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
