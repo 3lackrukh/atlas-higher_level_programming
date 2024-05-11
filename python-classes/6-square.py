42e04fafc100338d97d1be4971a914c88cea41ed
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 21:25:06 +0000

    added missing else: line 25

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -22,7 +22,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-            self.__size = size
+        else:
+            self.__size = size 
 
         if not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
