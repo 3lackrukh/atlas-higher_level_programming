af241ee36ccd03da3497c1acca3748f66e8049dc
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 19:24:36 +0000

    created 4-square.py and test file. Added property getter and setter.

---
+++ python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Module defines geometric class Square.
+"""
+
+
+class Square:
+    """
+    Class represents a square.
+
+    Attributes:
+        size (int): length of the sides.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize new Square instance.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        """
+        Method calculates the area of instance Square
+        """
+        return (self.__size * self.__size)
