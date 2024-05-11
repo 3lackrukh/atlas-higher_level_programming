6145d6f199d434fc185429b8bc48e85c3fa6d3af
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 20:02:15 +0000

    created 5-square.py and test file. Adds public instance method. prints square as a grid of '#'

---
+++ python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+Module defines geometric class Square.
+"""
+
+
+class Square:
+    """
+    Class represents a square.
+
+    Attributes:
+        size (int): length of the sides.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize new Square instance.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Method calculates the area of instance Square
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Method prints instance of Square
+        represented by '#'.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for k in range(0, self.__size):
+                    print("#", end="")
+                    print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
