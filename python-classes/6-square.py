427c82db67822e743ce7b049305889ac621ada0e
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 21:27:34 +0000

    Added additional edgecase check for tuple lines 28 and 52

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -25,7 +25,7 @@ class Square:
         else:
             self.__size = size 
 
-        if not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
+        if not isinstance(position, tuple) or not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         
         else:
@@ -49,7 +49,7 @@ class Square:
     
     @position.setter
     def position(self, position):
-        if not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
+        if not isinstance(position, tuple) or not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         
         else:
