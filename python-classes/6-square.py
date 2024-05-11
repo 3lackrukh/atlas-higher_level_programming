0dd6d47f8a3f6faf1cd5768ccb1d89453650b402
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 21:20:43 +0000

    restructured to place property getter-setter pairs above methods

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -18,13 +18,28 @@ class Square:
         """
         Initialize new Square instance.
         """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        if not isinstance(position, tuple) or position[0] < 0 or position[1] < 0 or position[1] == None:
+        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, position):
+        if not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         
         else:
@@ -51,26 +66,3 @@ class Square:
                     print("#", end="")
                 print()
 
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    @property
-    def position(self):
-        return self.__position
-    
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple) or value[0] < 0 or value[1] < 0 or value[1] == None:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        
-        else:
-            self.__position = value
