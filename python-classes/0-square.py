cbab4717c08fdd3c6764f4240c348082b06672a1
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 16:56:33 +0000

    Created 0-square.py and test file. Module defines a class, no attributes.

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 
+Module defines geometric class Square.
+Currently empty for exercise purposes.
+"""
+class Square:
+    """ 
+    Currently empty for ecercise purposes.
+    """
+    pass
