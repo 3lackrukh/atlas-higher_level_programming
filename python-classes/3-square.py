0a0cc31144af00f7f0da6871f0f89c6918c8b4ed
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-11 17:52:58 +0000

    created 3-square.py and test file. adds instance method returning the area of the square

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Module defines geometric class Square.
+"""
+
+
+class Square:
+    """
+    Class represents a square.
+
+    Attributes:
+        size (int): length of the sides.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize new Square instance.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Method calculates the area of instance Square
+        """
+        return (self.__size * self.__size)
