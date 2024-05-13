9c0e03f002d64ff5b66f8203c63b389e9e24ec9f
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-13 17:52:31 +0000

    double negative line 43?

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,7 +40,7 @@ class Square:
     
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or not all(isinstance(i, int) and 1 > 0 for i in value):
+        if not isinstance(value, tuple) or all(isinstance(i, int) and 1 > 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
