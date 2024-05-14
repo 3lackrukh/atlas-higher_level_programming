94e4f6aba0763cfcad22957207fbd153982b410b
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 23:07:06 +0000

    error handling also in setters

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -40,6 +40,10 @@ class Rectangle:
     
     @width.setter
     def width(self, width):
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
         self.__width = width
     
     @property
@@ -48,4 +52,8 @@ class Rectangle:
     
     @height.setter
     def height(self, height):
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
         self.__height = height
