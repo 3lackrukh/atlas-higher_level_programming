df0080aca50c1074813874fb3a1e2375aa0a41cf
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 16:33:15 +0000

    created 5-text_indentation.py, and corresponding doctest. checks for non-str parameter

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Module for the text_indentation method
+"""
+
+
+def text_indentation(text):
+
+    """
+    Prints a text with 2 new lines after each of the following chars
+    '.' '?' ':'
+
+    Parameters:
+        Text (str): A string of chars to be formatted and printed.
+
+    Raises:
+        TypeError: if text is not a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+  
+    for i in text:
+        if i in (".", "?", ":"):
+            print(i + "\n\n", end="")
+        else: 
+            print(i, end="")
