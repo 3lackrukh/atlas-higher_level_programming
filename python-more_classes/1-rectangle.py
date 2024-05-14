b1108b48a59fff968d0896577f4efa994202937d
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 23:16:05 +0000

    pycode style fixes removed whitespaces

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -30,27 +30,27 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-       
+
         self.__height = height
         self.__width = width
 
     @property
     def width(self):
         return (self.__width)
-    
+
     @width.setter
     def width(self, width):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        
+
         self.__width = width
-    
+
     @property
     def height(self):
         return (self.__height)
-    
+
     @height.setter
     def height(self, height):
         if not isinstance(height, int):
