f05ed85fd1487bcb31ed54f68eb8a55c68a0a719
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 22:48:48 +0000

    added getter and setter for attributes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,13 +22,30 @@ class Rectangle:
         """
         Initialize new Rectangle instance.
         """
+
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return (self.__width)
+    
+    @width.setter
+    def width(self, width):
         if width < 0:
             raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
+        self.__width = width
+    
+    @property
+    def height(self):
+        return (self.__height)
+    
+    @height.setter
+    def height(self, height):
+        if height < 0:
+            raise ValueError("height must be >= 0")
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         self.__height = height
-        self.__width = width
