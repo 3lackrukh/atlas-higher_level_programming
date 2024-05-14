2806d333130dfac980b67766104960871bf58371
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 20:54:11 +0000

    created 1-rectangle.py and test file. initializes with a width parameter

---
+++ python-classes/1-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module defines geometric class Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Class represents a Rectangle.
+
+    Attributes:
+        width (int): length of the x-axis.
+    """
+
+    def __init__(self, width):
+        """
+        Initialize new Rectangle instance.
+        """
+        self.__width = width
