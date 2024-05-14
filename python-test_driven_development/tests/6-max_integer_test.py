1f3878d62b17ebf4eb92f3839263902596a2045f
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 20:33:59 +0000

    added test_beginning instance. 1 test or three tests?

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import unittest
 
-def max_integer(list=[]):
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    def test_beginning(self):
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([3]), 3)
+        self.assertEqual(max_integer([]), None)
+
