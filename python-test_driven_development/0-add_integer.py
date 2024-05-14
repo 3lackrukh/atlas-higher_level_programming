b6aa94f5f7d9e8eeba19bd6fe11f5761fdb45833
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 00:44:08 +0000

    formatting corrections line 7 and 30

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 Module for add_integer method.
 """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers together.
@@ -26,5 +27,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, int):
         raise TypeError("b must be an integer")
-    
+
     return a + b
