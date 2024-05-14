6042fc6f176624ab8a3cb25894e9dc7133894e6c
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 23:41:02 +0000

    added perimiter condition attribute is 0

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -44,6 +44,8 @@ class Rectangle:
         """
         Method calculates the perimeter of instance Rectangle
         """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return (2 * (self.__width + self.__height))
         
     @property
