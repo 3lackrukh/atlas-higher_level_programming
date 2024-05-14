21b524ac27a489afcf5cb59d4250282b1158d495
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 04:11:33 +0000

    created 3-say_my_name.py and test file. checks normal functionality and non-string case

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module for the say_my_name module
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    concatenates str parameters into the string 'My name is <first name> <last name>'
+    
+    Parameters:
+        first_name(str): the first name to be concatenated 
+        last_name(str, optional): last name to be concatenated. Default "".
+    
+        Raises:
+            TypeError: if either parameter is not of type str.
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {} ".format(first_name))
