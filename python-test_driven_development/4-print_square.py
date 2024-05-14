d4d959a4052f8f31a3a3209b2e962a7ee69f666b
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-14 04:55:04 +0000

    Created 4-print_square.py and test file. checks for float, negative, and string parameters

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Module for the print_square method.
+"""
+
+
+def print_square(size):
+    """
+    prints a square gird of specified size
+    using '#'
+
+    Parameters:
+        size (int): dimensions of the square.
+    
+    Raises:
+        TypeError: if size is not of type int.
+        ValueError: if size < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(0, size):
+        for k in range(0, size):
+            print("#", end='')
+        print()
