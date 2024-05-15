acee792fde03e56964c976ca16141ce67ba11b90
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-15 16:34:28 +0000

    fixed indentation line 31

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-         if width < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
         if height < 0:
             raise ValueError("height must be >= 0")
