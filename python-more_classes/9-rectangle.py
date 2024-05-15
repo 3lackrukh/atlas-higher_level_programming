d4623f98c3d9357b8b4a1060bd5886c41fbc868b
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-15 16:31:35 +0000

    swapped order of height/width typeerror raising

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -24,14 +24,14 @@ class Rectangle:
         """
         Initialize new Rectangle instance.
         """
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
 
         self.height = height
         self.width = width
