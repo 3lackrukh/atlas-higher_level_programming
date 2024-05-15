098ea88a749685d12e080adba7fcfe21b1d5eb62
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-15 02:13:13 +0000

    ok, just whatever you want I guess

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -72,7 +72,7 @@ class Rectangle:
             return r_mtrx
 
     def __repr__(self):
-        return f"<3-rectangle.Rectangle object at{hex(id(self))}>"
+        return "Rectangle({}, {})".format(self.__width,self.__height)
 
     @property
     def width(self):
