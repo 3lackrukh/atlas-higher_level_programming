c139387c051416d4abe5cb47298da4569649d9d5
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-21 18:33:26 +0000

    created 8-rectangle.py and test file. Inherits from BG and validates height and width values

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    This  module defines a BaseGeometry sub-class, Rectangle.
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+"""
+    Class represents a Rectangle.
+
+    Attributes:
+        width (int): length of the x-axis. Default 0
+        height (int): length of the y-axis. Default 0
+
+    Methods:
+        area: inherited from BaseGeometry (not implemented)
+
+        integer_validator from BaseGeometry
+            - validates input is integer
+"""
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
