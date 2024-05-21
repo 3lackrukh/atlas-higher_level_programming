5d22cb2ca62659eeca2433013999b455bd78a93c
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-21 22:26:01 +0000

    PEP8 formatting removed whitespace line 13

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -10,7 +10,7 @@ def to_json_string(my_obj):
     """
         Parameters:
             my_obj: the object to be serialized into JSON format.
-        
+
         Returns: Serialized JSON representation of the object.
     """
     return json.dumps(my_obj)
