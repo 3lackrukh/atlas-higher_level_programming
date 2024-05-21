28ece36b9d4b59acb487303dbc151aee69d3083c
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-21 02:20:46 +0000

    does not use isinstance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,10 +6,10 @@
 
 def is_same_class(obj, a_class):
     """
-        Utilizes the isinstance() built-in method
+        Compares types with equality operator
 
         Returns:
-            True (bool): if the object is an instance of specified class.
+            True (bool): if the object is of specified class.
             False (bool): if not.
     """
     return (type(obj) == a_class)
