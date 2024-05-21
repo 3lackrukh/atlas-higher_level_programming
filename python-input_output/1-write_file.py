a21c24f99b914383a90229322b2d8db85030fcb3
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-21 22:04:54 +0000

    created 1-write_file.py, and 1-main.py test file.

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    This module contains a method which
+    writes a string to a text file
+    and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        Parameters:
+            filename (str): the name of the file to write to.
+            text (str): the text to be written to the file.
+            
+        Returns:
+            char_ct (int): the number of characters written.
+    """
+    char_ct = len(text)
+    with open(filename, "w") as f:
+        f.write(text)
+    return char_ct
