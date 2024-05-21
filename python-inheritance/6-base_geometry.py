45e7dfd83ab930bdabe90c79f25f3a56085cec45
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-21 03:02:14 +0000

    created 6-base_geometry.py and test file. Raises custom exception because it can't do anything else.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Module defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        Initial prototype for base class geometry
+    """
+
+    def area(self):
+        """
+            Initial prototype of area method.
+            not implemented.
+
+            Raises:
+                Exception with message
+        """
+        raise Exception("area() is not implemented")
