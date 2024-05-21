280d47d31ab1a55b0ad48c68a3dbc58f5b829cd0
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-21 22:25:03 +0000

    created 3-to_json_string.py and 3-main.py test file

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This module contains the to_json_string method which
+    returns the JSON representation of a specified object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+        Parameters:
+            my_obj: the object to be serialized into JSON format.
+        
+        Returns: Serialized JSON representation of the object.
+    """
+    return json.dumps(my_obj)
