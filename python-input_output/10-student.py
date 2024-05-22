6b961ffb5b751a71873b9c6b77db55f91f9ef099
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-22 00:24:01 +0000

    created 10-student.py based on 9-student. added attribute search to public method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    This module defines a python class Student
+"""
+
+
+class Student:
+    """
+        Attributes:
+            - first_name (str)
+            - last_name (str)
+            - age (int)
+        Methods:
+            - to_json: retrieves dictionary representation of attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items()if k in attrs}
