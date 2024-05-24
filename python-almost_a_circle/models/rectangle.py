628e57967d104acc1ccca5c111057b0c5ba513ee
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-24 23:50:12 +0000

    String construction shortens line 102

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,5 +99,6 @@ class Rectangle(Base):
                 print()
 
     def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        string = f"[Rectangle] ({self.id})"
+        string += f"{self.x}/{self.y} - {self.width}/{self.height}"
         return string
