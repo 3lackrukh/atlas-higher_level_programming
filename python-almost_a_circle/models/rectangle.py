13441492fcbc3b570e4dc7397acf13d117012bdf
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-24 23:47:17 +0000

    Added __str__ method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,7 @@ class Rectangle(Base):
                 for k in range(0, self.__width):
                     print("#", end="")
                 print()
+
+    def __str__(self):
+        return f"[Rectangle] {self.id} {self.x}/{self.y} - {self.width}/{self.height}"
+        return string
