1397f010c50ca6edb438f7da4069c3c6c05d2cb3
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-24 20:35:05 +0000

    Ready to check

--- python-almost_a_circle/1-main.py
+++ python-almost_a_circle/1-main.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     r2 = Rectangle(2, 10)
     print(r2.id)
 
-    r3 = Rectangle(10, 2, 0, 0, 12)
+    r3 = Rectangle(10, 2, 7, 7, 12)
     print(r3.id)
+    print(r3.x)
