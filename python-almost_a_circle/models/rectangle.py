f0da6336b1743fe01a7022fda2faab86e6234da5
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-25 03:17:54 +0000

    reverted to expanded update logic lines 112-121

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -109,9 +109,16 @@ class Rectangle(Base):
             Method updates instance parameters.
         """
         if args and args is not None:
-            for key, value in zip(['id', 'width', 'x', 'y'], args):
-                if hasattr(self, key):
-                    setattr(self, key, value)
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
         else:
             for key, value in kwargs.items():
                 if hasattr(self, key):
