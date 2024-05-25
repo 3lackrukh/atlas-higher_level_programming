bd19b1d3923f5b0481e8787e26790723a6079df6
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-25 17:19:48 +0000

    Added size setter and getter lines 16-26 sets inherited width and height attributes and depends on their error raising

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,6 +13,17 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
+    
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, size):
+        self.__width = size
+        self.__height = size
+
 
     def __str__(self):
         """
