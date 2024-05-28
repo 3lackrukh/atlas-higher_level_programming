9e4f5f52979a7850db21fbe533d9628f80d1128b
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-28 16:49:38 +0000

    separated test id into 2 tests for specified id and empty

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -11,10 +11,14 @@ class TestBase(unittest.TestCase):
     def test_base_id(self):
         """
             Test ensures user specified id functionality
-            and auto id functionality 
         """
         b1 = Base(12)
         self.assertEqual(b1.id, 12)
+
+    def test_base_id_empty(self):
+        """
+            Test ensures automatic id assignment
+        """
         b2 = Base()
         b3 = Base()
         self.assertEqual(b2.id, 1)
