7d74d6885746b4a5a18b6c4409aef08ee07d9bf6
3lackrukh <nathan.rhys@atlasschool.comls>
2024-05-29 02:52:08 +0000

    what is going wrong?

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-""" Unittest module for Base class """
+"""unittests for base.py"""
 
-import unittest
 import os
-import json
+import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
 
-class TestBase(unittest.TestCase):
-    """This module defines test methods for the object class Base."""
-    def setUp(self):
-        """This method resets object counter to 0"""
-        Base._Base__nb_objects = 0
+class Test_Base(unittest.TestCase):
+    """test the class Base"""
 
     def test_id(self):
-        """Test ensures automatic id assignment"""
+        """can we make a new Base"""
+        Base._Base__nb_objects = 0
         base = Base()
         self.assertEqual(base.id, 1)
+
+    def test_make_a_base(self):
+        """do the id's make sense"""
+        baseA = Base()
+        baseB = Base()
+        self.assertEquals(baseA.id, baseB.id - 1)
+
+    def test_assign_id(self):
+        """can we assign an id ourselves"""
+        self.assertEqual(98, Base(98).id)
+
+    def test_makes_a_base(self):
+        """another test of making a new Base with default id"""
+        self.assertIsNotNone(Base())
+
