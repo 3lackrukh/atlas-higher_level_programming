8090d720b7238ddc1ed7c48af38d5261458f695e
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-29 16:44:27 +0000

    PLD setup for an upcoming project

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,21 +108,42 @@ class Rectangle(Base):
         """
             Method updates instance parameters.
         """
-        if args and args is not None:
-            if len(args) > 0:
+        if args:
+            try:
                 self.id = args[0]
-            if len(args) > 1:
+            except Exception:
+                pass
+            try:
                 self.width = args[1]
-            if len(args) > 2:
+            except Exception:
+                pass
+            try:
                 self.height = args[2]
-            if len(args) > 3:
+            except Exception:
+                pass
+            try:
                 self.x = args[3]
-            if len(args) > 4:
+            except Exception:
+                pass
+            try:
                 self.y = args[4]
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+            except Exception:
+                pass
+        # if args:
+        #     if len(args) > 0:
+        #         self.id = args[0]
+        #     if len(args) > 1:
+        #         self.width = args[1]
+        #     if len(args) > 2:
+        #         self.height = args[2]
+        #     if len(args) > 3:
+        #         self.x = args[3]
+        #     if len(args) > 4:
+        #         self.y = args[4]
+         else:
+             for key, value in kwargs.items():
+                 if hasattr(self, key):
+                     setattr(self, key, value)
 
     def to_dictionary(self):
         """
