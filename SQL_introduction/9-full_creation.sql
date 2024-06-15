3afc0ceaa6a3ce26e2aa1636abf9ed2ac266ef39
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-15 23:02:23 +0000

    values is a list of lists

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Attempt to create second_table in the specified database
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+-- create records for John, Alex, Bob, George
+INSERT INTO  second_table (id, name, score)
+VALUES 
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
