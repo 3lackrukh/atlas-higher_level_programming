46c876388c50cf47ba823c1ecd8b6743c082c5e4
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-17 13:03:20 -0500

    added database change line 5

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- creates database hbtn_0d_usa if it doesnt exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- change database
+USE hbtn_0d_usa
+
 -- creates table states in hbtn_0d_usa if it doesnt exist
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
