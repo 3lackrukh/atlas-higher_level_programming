7961c8d74340708af33cbc6bda32633c4e8ceb83
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-17 01:03:41 -0500

    creates database and table states , unique ids and name not null

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- creates database hbtn_0d_usa if it doesnt exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- creates table states in hbtn_0d_usa if it doesnt exist
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
