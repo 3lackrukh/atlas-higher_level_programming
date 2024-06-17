82aa77a734a9744e12928f574249b76ee74710da
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-17 12:56:33 -0500

    corrected lowercase keyword line 2

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres and number of shows linked to them
-SELECT tv_genres.name as genre, 
+SELECT tv_genres.name AS genre, 
 COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
