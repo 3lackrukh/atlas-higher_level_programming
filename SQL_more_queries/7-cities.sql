b5472f6938e00eece267af34db1ce388f5c7330b
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-17 10:18:57 -0500

    removed USE stmts on files 6, 7, 8

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- creates table cities in hbtn_0d_usa if it doesnt exist
-USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- query lists cities of California in database hbtn_0d_usa
-USE hbtn_0d_usa;
 SELECT c.id, c.name
 FROM cities c
 WHERE c.state_id IN (
