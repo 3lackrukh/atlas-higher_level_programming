bd31e879f28bd25d97934215a2ed7b1e54a6e95f
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-17 12:51:14 -0500

    changed alias line 2

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in database hbtn_0d_usa
-SELECT c.id, c.name, s.name AS state_name
+SELECT c.id, c.name, s.name AS name
 FROM cities c
 INNER JOIN states s ON c.state_id = s.id
 ORDER BY  c.id ASC;
