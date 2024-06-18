040bbe331c36f15c4806b9d81d6272eb88381f39
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-18 10:04:08 -0500

    connect, execute print results row by row

---
+++ pythin_object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa:
+
+Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+        username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+
+    cursor = db.cursor()
+
+    cursor.execute(SELECT * FROM 'states' ORDER BY 'id')
+
+    results = cursor.fetchall()
+
+    FOR row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
