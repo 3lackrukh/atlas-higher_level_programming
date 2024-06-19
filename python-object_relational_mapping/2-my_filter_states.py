cefdbadc1875ba21d4f56e737ff9b99f7605d9fb
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-19 17:05:30 -0500

    added query constructor inserts user specified state

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+SQLPython - Object-relational mapping (Project 2193, Task 2)
+
+Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+
+* Your script should take 4 arguments:
+mysql username, mysql password, database name and state name searched
+(no argument validation needed)
+
+* You must use the module MySQLdb (import MySQLdb)
+
+* Your script should connect to a MySQL server running on localhost at port 3306
+
+* You must use format to create the SQL query with the user input
+
+* Results must be sorted in ascending order by states.id
+
+Your code should not be executed when imported
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Get command-line arguments
+    username, password, db_name, state_name = (
+    sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # prepare SQL query to accept the state_name argument
+    query = (
+        f"SELECT * FROM `states` "
+        "WHERE `name` = %s "
+        "ORDER BY `id` ASC")
+    # Execute the query
+    cursor.execute(query, (state_name,))
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Print the results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
