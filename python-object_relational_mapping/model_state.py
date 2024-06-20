b439210bf36858ea06b46d869c1d344a8ed3c6f2
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-19 19:02:35 -0500

    I think I did this right...

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,36 @@
+"""
+Model for the States table in a MySQL database.
+
+This module defines a SQLAlchemy model for the 'states' table
+includes a State class
+that inherits from a declarative base.
+
+The State class represents a row in the 'states'
+table, with attributes corresponding to columns in the table.
+
+Classes:
+    State(Base): Represents a state in the 'states' table.
+    Base: The declarative base class for all models in this application.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Represents a state in the 'states' table.
+
+    Attributes:
+        id (int): Auto-incremented primary key.
+        name (str): Non-null string maximum length of 128 characters.
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, 
+                primary_key=True,
+                autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
