f7fb0c735c7bdcb9186d57b2b3bb090d1bdc362e
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-18 12:19:44 -0500

    styling with semistandard --fix

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
 for (i = 0; i < myVar.length; i++) {
-    console.log(myVar[i]);
+  console.log(myVar[i]);
 }
