6a4400bbddb6ac55271252a26744e963d125efce
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-18 12:15:56 -0500

    adds file for task 1 const array iterated through with a let integer

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myVar = ["C is fun", "Python is cool", "Javascript is amazing"];
+
+let i = 0;
+for (i = 0; i < myVar.length; i++)
+    {
+        console.log(myVar[i]);
+    }
