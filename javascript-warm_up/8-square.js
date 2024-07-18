ccebeb6610c7ddbe86acc916ea423c1993d1c612
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-18 14:28:03 -0500

    reverted

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 if (!isNaN(Number(process.argv[2]))) {
-  var i = process.argv[2];
+  let i = process.argv[2];
   while (i > 0) {
-    xString = '';
-    var k = process.argv[2];
+    let xString = '';
+    let k = process.argv[2];
     while (k > 0) {
       xString += 'X';
       k--;
