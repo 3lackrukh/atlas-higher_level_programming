905b54d80951222e9687f1016056c011d07557cc
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-19 23:05:45 -0500

    creates file for task 13. corrects conditional logic for 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
 function add (a, b) {
-  if (!isNaN(a) || !isNaN(b)) {
+  if (!isNaN(a) && !isNaN(b)) {
     const sum = a + b;
     console.log(sum);
   } else {
